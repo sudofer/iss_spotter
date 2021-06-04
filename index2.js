4ec61f3fc38793faec52136896c4b1fc7a0fc629
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-04 01:36:50 +0000

    implemented promises...needs revisit

---
+++ index2.js
@@ -0,0 +1,11 @@
+// index2.js
+
+const { nextISSTimesForMyLocation, printPassTimes } = require("./iss_promised");
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
